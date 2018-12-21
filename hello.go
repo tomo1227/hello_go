3c8bfd042d6a4073e5518ef04c807c19f05b97f3
Tomoki <tomo.soccer1227@gmail.com>
2018-12-22 04:50:06 +0900

    first pacage

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tomo1227/stringutil"
+)
 
 func main(){
-	fmt.Printf("Hello, world.\n")
+	fmt.Println(stringutil.Reverse("!dlrow ,olleH"))
 }
